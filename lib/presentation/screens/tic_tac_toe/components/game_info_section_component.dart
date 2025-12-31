0eff2224b20a746f6f7f447414b4e1b009de2ae1
kalebe <kalebemisael@gmail.com>
2025-12-31 17:06:20 -0300

    Add ScoreDisplayComponent to LocalGameScreen and refactor GameInfoSectionComponent
    - Introduced ScoreDisplayComponent to visually represent player scores in the LocalGameScreen, enhancing gameplay feedback.
    - Refactored GameInfoSectionComponent to simplify the display of the current round, improving code clarity and maintainability.

--- lib/presentation/screens/tic_tac_toe/components/game_info_section_component.dart
+++ lib/presentation/screens/tic_tac_toe/components/game_info_section_component.dart
@@ -11,36 +11,9 @@ class GameInfoSectionComponent extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       margin: const EdgeInsets.only(bottom: 10),
-      child: Column(
-        children: [
-          Text(
-            'Round ${game.currentRound}/${game.maxRounds}',
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'X: ${game.scoreX}',
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
-              const SizedBox(width: 20),
-              Text(
-                'O: ${game.scoreO}',
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red,
-                ),
-              ),
-            ],
-          ),
-        ],
+      child: Text(
+        'Round ${game.currentRound}/${game.maxRounds}',
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
       ),
     );
   }
