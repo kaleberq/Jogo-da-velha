aa3e4f00c121117dc52534eac9e36a29785985d8
kalebe <kalebemisael@gmail.com>
2026-01-12 10:03:54 -0300

    feat: update app title localization and import app location extension

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_design_system/themes/ds_theme.dart';
+import 'package:jogo_da_velha/extensions/app_location_extension.dart';
 import 'package:jogo_da_velha/l10n/app_localizations.dart';
 import 'package:jogo_da_velha/presentation/screens/splash/splash_screen.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Jogo da Velha',
+      onGenerateTitle: (context) => context.l10n.appTitle,
       theme: DSTheme.light(),
       darkTheme: DSTheme.dark(),
       themeMode: ThemeMode.system,
