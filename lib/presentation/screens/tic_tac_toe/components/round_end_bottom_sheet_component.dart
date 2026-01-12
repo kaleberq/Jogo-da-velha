c2c13945b8c7af2fc653390352f2180dcd8e3cde
kaleberq <kalebemisael@gmail.com>
2026-01-11 21:29:49 -0300

    feat: change bottomsheet

--- lib/presentation/screens/tic_tac_toe/components/round_end_bottom_sheet_component.dart
+++ lib/presentation/screens/tic_tac_toe/components/round_end_bottom_sheet_component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_design_system/flutter_design_system.dart';
 import 'package:jogo_da_velha/domain/enums/player_enum.dart';
 
 class RoundEndBottomSheet extends StatelessWidget {
@@ -15,45 +16,20 @@ class RoundEndBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      decoration: BoxDecoration(
-        color: roundWinner == PlayerEnum.x
-            ? Colors.blue.shade700
-            : roundWinner == PlayerEnum.o
-            ? Colors.red.shade700
-            : Colors.grey.shade700,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.2),
-            blurRadius: 4,
-            offset: const Offset(0, 2),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: DSSpacing.lg,
+        vertical: DSSpacing.md,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(child: Text(roundEndMessage)),
+          TextButton(
+            onPressed: onNextRound,
+            child: const Text('Próximo Round'),
           ),
         ],
-      ),
-      child: SafeArea(
-        top: false, // evita espaçamento desnecessário no topo
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Text(
-                roundEndMessage,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: onNextRound,
-              style: TextButton.styleFrom(foregroundColor: Colors.white),
-              child: const Text('Próximo Round'),
-            ),
-          ],
-        ),
       ),
     );
   }
