b7931f289236a9da7b5fa454f881be1eed7c55e0
kaleberq <kalebemisael@gmail.com>
2026-01-16 18:26:56 -0300

    feat: implement styles color

--- lib/presentation/screens/tic_tac_toe/components/score_display_component.dart
+++ lib/presentation/screens/tic_tac_toe/components/score_display_component.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_design_system/flutter_design_system.dart';
+import 'package:jogo_da_velha/app/theme/app_colors.dart';
 import 'package:jogo_da_velha/data/models/tic_tac_toe_game_model.dart';
 import 'package:jogo_da_velha/domain/enums/player_enum.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -17,13 +18,15 @@ class ScoreDisplayComponent extends StatelessWidget {
         Expanded(
           flex: 1,
           child: Container(
-            padding: const EdgeInsets.all(DSSpacing.md),
+            constraints: BoxConstraints(minHeight: 50),
+            padding: const EdgeInsets.all(DSSpacing.xs),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(DSRadius.sm),
-              border: Border.all(color: DSColors.primary),
+              borderRadius: BorderRadius.circular(DSRadius.md),
+              border: Border.all(color: AppColors.playerX, width: 5),
             ),
-            child: Center(
-              child: Row(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Wrap(
                 children: List.generate(
                   game.scoreX,
                   (_) => SvgPicture.asset(
@@ -31,7 +34,7 @@ class ScoreDisplayComponent extends StatelessWidget {
                     width: 24,
                     height: 24,
                     colorFilter: const ColorFilter.mode(
-                      DSColors.primary,
+                      AppColors.playerX,
                       BlendMode.srcIn,
                     ),
                   ),
@@ -43,13 +46,15 @@ class ScoreDisplayComponent extends StatelessWidget {
         Expanded(
           flex: 1,
           child: Container(
-            padding: const EdgeInsets.all(DSSpacing.md),
+            constraints: BoxConstraints(minHeight: 50),
+            padding: const EdgeInsets.all(DSSpacing.xs),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(DSRadius.sm),
-              border: Border.all(color: DSColors.primary),
+              borderRadius: BorderRadius.circular(DSRadius.md),
+              border: Border.all(color: AppColors.playerO, width: 5),
             ),
-            child: Center(
-              child: Row(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Wrap(
                 children: List.generate(
                   game.scoreO,
                   (_) => SvgPicture.asset(
@@ -57,7 +62,7 @@ class ScoreDisplayComponent extends StatelessWidget {
                     width: 24,
                     height: 24,
                     colorFilter: const ColorFilter.mode(
-                      DSColors.primary,
+                      AppColors.playerO,
                       BlendMode.srcIn,
                     ),
                   ),
